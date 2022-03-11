029e2fb498e0fd5e3d1772e299f1ea050a7ba1b9
SARATH JOSEPH <sarathcjoseph@gmail.com>
2022-03-11 12:23:11 -0500

    Update basepointer.asm

--- basepointer.asm
+++ basepointer.asm
@@ -29,4 +29,4 @@ func:
 	
 	mov esp, ebp   ; restore stack pointer 
 	pop ebp        ; restore base pointer
-	ret            ; pops stack pointer and does a jump to that address
+	ret            ; pops return address off the stack to the instruction pointer
